3b85fc35f5d86e527b895c4d20f6a53e3970c618
eliozashvili <ilivhsazoile.g@gmail.com>
2026-07-25 21:10:13 +0400

    feat: freeze camera position on player death, fix weapon camera warning

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private Transform weaponCamera;
     [SerializeField] private int playerHealth;
 
     public void TakeDamage(int damage)
@@ -10,9 +12,22 @@ public class PlayerHealth : MonoBehaviour
 
         if (playerHealth <= 0)
         {
+            DeathCamera();
             Destroy(gameObject);
         }
 
         Debug.Log(playerHealth);
+    }
+    // Freezes camera where player was looking at the moment of death
+    private void DeathCamera()
+    {
+        Camera mainCam = Camera.main;
+
+        if (!mainCam) return;
+
+        if (mainCam.TryGetComponent(out CinemachineBrain brain))
+            brain.enabled = false;
+
+        weaponCamera.parent = null;
     }
 }
