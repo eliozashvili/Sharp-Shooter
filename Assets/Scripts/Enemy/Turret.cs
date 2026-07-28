6ca0663a89b467d104b3363556ab4163c966652b
eliozashvili <ilivhsazoile.g@gmail.com>
2026-07-28 12:01:40 +0400

    feat: turret projectile spawn

--- Assets/Scripts/Enemy/Turret.cs
+++ Assets/Scripts/Enemy/Turret.cs
@@ -1,12 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] private Transform turretTower;
     [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float spawnInterval;
+
+    private PlayerHealth _playerHealth;
+
+    private void Start()
+    {
+        _playerHealth = FindAnyObjectByType<PlayerHealth>();
+
+        StartCoroutine(FireProjectile());
+    }
 
     private void Update()
     {
         turretTower.LookAt(playerPosition);
+    }
+
+    private IEnumerator FireProjectile()
+    {
+        var wait = new WaitForSeconds(spawnInterval);
+
+        while (_playerHealth && _playerHealth.PlayerCurrentHealth > 0)
+        {
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, turretTower.rotation);
+
+            yield return wait;
+        }
     }
 }
